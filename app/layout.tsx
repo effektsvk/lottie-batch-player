a0c69083e4a15d01444d9965325ec6349a1f49a4
Erik Slovák <erik.slovak7@gmail.com>
2024-08-10 00:56:29 +0200

    chore: add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import DragAndDropProvider from "./DragAndDropProvider";
@@ -31,6 +32,7 @@ export default function RootLayout({
           </span>
           <span>v{packageJson.version}</span>
         </div>
+        <Analytics />
       </body>
     </html>
   );
